b0e585e47491edf8ce73aa203deb3498bda73d77
tomas <tomas@TOMAS-PC>
2018-09-14 18:47:23 +0200

    Add project files.

---
+++ MobileCoverageMapApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MobileCoverageMapApp.Startup))]
+namespace MobileCoverageMapApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
